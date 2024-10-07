dc665948fdf56c8a133d99c3bd41e7182a90c8a0
Chris Gillis <chris85gillis@gmail.com>
2024-10-07 17:44:12 -0500

    Wasn't getting last check had to tweek code to pickup the tripple a's

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,23 +1,25 @@
-String isPalindrome(String s) {
+// Check if a string is a palindrome and its length is at least 3
+bool isPalindrome(String s) {
   if (s.length < 3) {
-    return "none";
+    return false;
   }
   String reversed = s.split('').reversed.join('');
-  return s == reversed ? s : "none";
+  return s == reversed;
 }
 
 String longestPalindrome(String s) {
-  String longest = "none";
+  String longest = "";
 
   for (int i = 0; i < s.length; i++) {
-    for (int j = i + 3; j <= s.length; j++) {
-      String substring = s.substring(i, j);
+    for (int j = i + 2; j < s.length; j++) { // Ensure length >= 3
+      String substring = s.substring(i, j + 1);
 
-      if (isPalindrome(substring) != "none" && substring.length > longest.length) {
+      if (isPalindrome(substring) && substring.length > longest.length) {
         longest = substring;
       }
     }
   }
 
-  return longest;
+  // If no palindrome of length >= 3 found, return "none"
+  return longest.isEmpty ? "none" : longest;
 }
