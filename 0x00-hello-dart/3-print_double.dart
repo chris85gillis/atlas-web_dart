d5b4d8881f7fbc46605252bbe1353ca40bfafa19
Chris Gillis <chris85gillis@gmail.com>
2024-10-07 12:44:28 -0500

    Task 3, Print Doubles

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 void main() {
-  var number = 3.14159;
-  print('Double: ${number.toStringAsFixed(2)}\n');
+  print('Enter a number:');
+  var input = stdin.readLineSync();  // Read user input from the console
+  if (input != null) {
+    var number = double.parse(input);  // Parse the input as a double
+    print('Double: ${number.toStringAsFixed(2)}');
+  }
 }
