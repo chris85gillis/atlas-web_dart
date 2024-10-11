2202e031f6bbcd3fdc0b4504aad425ad1a02cf5e
Chris Gillis <chris85gillis@gmail.com>
2024-10-11 15:20:17 -0500

    Task 4, Mutables

---
+++ 0x02-dart-classes/4-main.dart
@@ -0,0 +1,14 @@
+// 4-main.dart
+import '4-mutables.dart';
+
+void main() {
+  final password = Password(password: "Passwordecode");
+  
+  print(password.isValid());
+  print(password.toString());
+  
+  password.password = "Youssef4321";
+  
+  print(password.isValid());
+  print(password.toString());
+}
