c4d1b23562017131db2d4075365ecc9b919d8f46
Chris Gillis <chris85gillis@gmail.com>
2024-10-11 15:10:11 -0500

    Task 2, Password class

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,19 @@
+// 2-password.dart
+class Password {
+  String password = '';
+
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+    bool hasUppercase = password.contains(RegExp(r'[A-Z]'));
+    bool hasLowercase = password.contains(RegExp(r'[a-z]'));
+    bool hasDigit = password.contains(RegExp(r'[0-9]'));
+    return hasUppercase && hasLowercase && hasDigit;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
